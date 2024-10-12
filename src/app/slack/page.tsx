8511b414db1e22b5ad8f00b63165b1f113d8d693
Kavin Valli <mail@kavin.me>
2024-10-12 16:57:39 -0700

    starter

--- src/app/slack/page.tsx
+++ src/app/slack/page.tsx
@@ -1,19 +1,7 @@
-import { SignOutBtn } from "@/components/SignOutBtn";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import NotConnected from "./NotConnected";
-import { getSlackChannels } from "../actions/slack";
-import Connected from "./Connected";
-import PostTestMessage from "./PostTestMessage";
 
 const SlackPage = async () => {
   const session = await getAuthSession();
@@ -27,28 +15,13 @@ const SlackPage = async () => {
 
   if (!user) return redirect("/");
 
-  const channels = await getSlackChannels(user.slack_access_token ?? "");
-
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center">
       <Card className="w-full max-w-lg">
         <CardHeader>
           <CardTitle>Slack</CardTitle>
-          <CardDescription>Slack</CardDescription>
         </CardHeader>
-        <CardContent>
-          {user.slack_access_token ? (
-            <div className="flex flex-col gap-4">
-              <Connected
-                channels={channels}
-                defaultChannelId={user.slack_channel_id ?? ""}
-              />
-              <SignOutBtn />
-            </div>
-          ) : (
-            <NotConnected />
-          )}
-        </CardContent>
+        <CardContent></CardContent>
       </Card>
     </div>
   );
